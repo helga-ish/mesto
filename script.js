547517a388b106a6d07761697332869cdf7b6666
helga-ish <belfegor.nomnom@gmail.com>
2022-12-20 17:28:12 +0800

    refactor: form rename into popup

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 let editButton = document.querySelector('.profile__edit-button');
-let formElement = document.querySelector('.form');
-let formToggle = document.querySelector('.form__toggle');
+let popupElement = document.querySelector('.popup');
+let popupToggle = document.querySelector('.popup__toggle');
 
 let submitButton = document.querySelector('.edit-form__button');
 let inputName = document.querySelector('.edit-form__field_name');
@@ -9,16 +9,16 @@ let profileName = document.querySelector('.profile__field-name');
 let profileAbout = document.querySelector('.profile__field-about');
 
 function popupOpen() {
-    formElement.classList.add('form_active');
+    popupElement.classList.add('popup_active');
     inputName.value = profileName.textContent;
     inputAbout.value = profileAbout.textContent;
 };
 editButton.addEventListener('click', popupOpen);
 
 function popupClose() {
-    formElement.classList.remove('form_active');
+    popupElement.classList.remove('popup_active');
 };
-formToggle.addEventListener('click', popupClose);
+popupToggle.addEventListener('click', popupClose);
 
 
 
@@ -29,4 +29,4 @@ function handleFormSubmit (evt) {
     profileAbout.textContent = inputAbout.value;
     popupClose();
 }
-formElement.addEventListener('submit', handleFormSubmit);
+popupElement.addEventListener('submit', handleFormSubmit);
