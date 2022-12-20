87fec12361c761b80fe5c575a25a2ee0a23f63cb
helga-ish <belfegor.nomnom@gmail.com>
2022-12-20 17:23:40 +0800

    feat: submit button interactive

--- script.js
+++ script.js
@@ -2,8 +2,16 @@ let editButton = document.querySelector('.profile__edit-button');
 let formElement = document.querySelector('.form');
 let formToggle = document.querySelector('.form__toggle');
 
+let submitButton = document.querySelector('.edit-form__button');
+let inputName = document.querySelector('.edit-form__field_name');
+let inputAbout = document.querySelector('.edit-form__field_about');
+let profileName = document.querySelector('.profile__field-name');
+let profileAbout = document.querySelector('.profile__field-about');
+
 function popupOpen() {
     formElement.classList.add('form_active');
+    inputName.value = profileName.textContent;
+    inputAbout.value = profileAbout.textContent;
 };
 editButton.addEventListener('click', popupOpen);
 
@@ -11,3 +19,14 @@ function popupClose() {
     formElement.classList.remove('form_active');
 };
 formToggle.addEventListener('click', popupClose);
+
+
+
+
+function handleFormSubmit (evt) {
+    evt.preventDefault();
+    profileName.textContent = inputName.value;
+    profileAbout.textContent = inputAbout.value;
+    popupClose();
+}
+formElement.addEventListener('submit', handleFormSubmit);
