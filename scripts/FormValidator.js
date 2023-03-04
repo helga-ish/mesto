4708722d82afda59876127b7ab01c0a297819f74
helga-ish <belfegor.nomnom@gmail.com>
2023-03-04 21:00:39 +0800

    refact: class validator finished

--- scripts/FormValidator.js
+++ scripts/FormValidator.js
@@ -4,22 +4,21 @@ class FormValidator {
     this._form = form;
   }
 
-   _showFieldError = (inputEl, errorMessage) => {
+  _showFieldError = (inputEl, errorMessage) => {
     const errorEl = this._form.querySelector(`.${inputEl.id}-error`);
     inputEl.classList.add(this._object.formFieldTypeErrorClass);
     errorEl.classList.add(this._object.activeErrorClass);
     errorEl.textContent = errorMessage;
-
   };
   
-   _hideFieldError = (inputEl) => {
+  _hideFieldError = (inputEl) => {
       const errorEl = this._form.querySelector(`.${inputEl.id}-error`);
       inputEl.classList.remove(this._object.formFieldTypeErrorClass);
       errorEl.classList.remove(this._object.activeErrorClass);
       errorEl.textContent = '';
     };
   
-   _checkInputValidity = (inputEl) => {
+  _checkInputValidity = (inputEl) => {
         if (!inputEl.validity.valid) {
           this._showFieldError(inputEl, inputEl.validationMessage);
         } else {
@@ -27,13 +26,13 @@ class FormValidator {
         }
       };
   
-   _hasInvalidInput = (fieldList) => {
+  _hasInvalidInput = (fieldList) => {
     return fieldList.some((inputEl) => {
       return !inputEl.validity.valid;
     });
   };
   
-   _toggleButtonState = (fieldList, buttonEl) => {
+  _toggleButtonState = (fieldList, buttonEl) => {
     if (this._hasInvalidInput(fieldList)) {
       buttonEl.classList.add(this._object.inactiveButtonClass);
       buttonEl.disabled = true;
@@ -43,106 +42,24 @@ class FormValidator {
     };
   };
   
-   _setEventListeners = () => {
+  _setEventListeners = () => {
     const fieldList = Array.from(this._form.querySelectorAll(this._object.formFieldSelector));
     const submitButtonEl = (this._form.parentNode).querySelector(this._object.buttonSelector);
     this._toggleButtonState(fieldList, submitButtonEl);
     fieldList.forEach((inputEl) => {
-      inputEl.addEventListener('input', function () {
+      inputEl.addEventListener('input', () => {
         this._checkInputValidity(inputEl);
-        _toggleButtonState(fieldList, submitButtonEl);
+        this._toggleButtonState(fieldList, submitButtonEl);
       });
     });
-
   };
   
   enableValidation = () => {
         this._form.addEventListener('submit', function(evt) {
-              evt.preventDefault();
-            });
-        const fieldsetList = Array.from(this._form.querySelectorAll(this._object.formFieldsetSelector));
-            fieldsetList.forEach((fieldset) => {
-            this._setEventListeners(fieldset);
-            });
+          evt.preventDefault();
+        });
         this._setEventListeners();
       };
-};
+  };
 
 export { FormValidator };
-
-
-// const showFieldError = (formEl, inputEl, errorMessage) => {
-//     const errorEl = formEl.querySelector(`.${inputEl.id}-error`);
-//     inputEl.classList.add(allSelectors.formFieldTypeErrorClass);
-//     errorEl.classList.add(allSelectors.activeErrorClass);
-//     errorEl.textContent = errorMessage;
-//   };
-  
-//   const hideFieldError = (formEl, inputEl) => {
-//       const errorEl = formEl.querySelector(`.${inputEl.id}-error`);
-//       inputEl.classList.remove(allSelectors.formFieldTypeErrorClass);
-//       errorEl.classList.remove(allSelectors.activeErrorClass);
-//       errorEl.textContent = '';
-//     };
-  
-//   const checkInputValidity = (formEl, inputEl) => {
-//         if (!inputEl.validity.valid) {
-//           showFieldError(formEl, inputEl, inputEl.validationMessage);
-//         } else {
-//           hideFieldError(formEl, inputEl);
-//         }
-//       };
-  
-//   const hasInvalidInput = (fieldList) => {
-//     return fieldList.some((inputEl) => {
-//       return !inputEl.validity.valid;
-//     });
-//   };
-  
-//   const toggleButtonState = (fieldList, buttonEl) => {
-//     if (hasInvalidInput(fieldList)) {
-//       buttonEl.classList.add(allSelectors.inactiveButtonClass);
-//       buttonEl.disabled = true;
-//     } else {
-//       buttonEl.classList.remove(allSelectors.inactiveButtonClass);
-//       buttonEl.disabled = false;
-//     };
-//   };
-  
-//   const setEventListeners = (formEl) => {
-//     const fieldList = Array.from(formEl.querySelectorAll(allSelectors.formFieldSelector));
-//     const submitButtonEl = (formEl.parentNode).querySelector(allSelectors.buttonSelector);
-//     toggleButtonState(fieldList, submitButtonEl);
-//     fieldList.forEach((inputEl) => {
-//       inputEl.addEventListener('input', function () {
-//         checkInputValidity(formEl, inputEl);
-//         toggleButtonState(fieldList, submitButtonEl);
-//       });
-//     });
-//   };
-  
-//   const enableValidation = () => {
-//     const formList = Array.from(document.querySelectorAll(allSelectors.formSelector));
-//       formList.forEach((formEl) => {
-//         formEl.addEventListener('submit', function(evt) {
-//           evt.preventDefault();
-
-//         });
-//         const fieldsetList = Array.from(formEl.querySelectorAll(allSelectors.formFieldsetSelector));
-//        fieldsetList.forEach((fieldset) => {
-//          setEventListeners(fieldset);
-//        });
-//       });
-//   };
-//   enableValidation(
-//     allSelectors = {
-//     formSelector: '.form',
-//     formFieldsetSelector: '.form__fields',
-//     formFieldSelector: '.form__field',
-//     buttonSelector: '.form-button',
-//     inactiveButtonClass: 'form-button_inactive',
-//     activeErrorClass: 'form__field-error_active',
-//     formFieldTypeErrorClass: 'form__field_type_error',
-//   }); 
-
-  
