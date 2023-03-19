b2a36b390610ea6d8ad2eb92a8953ffeb8253b15
Helga Ish <belfegor.nomnom@gmail.com>
2023-03-20 00:39:24 +0800

    feat: webpack and babel installed

---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+const path = require('path');
+
+module.exports = {
+    entry: { main: './src/index.js' },
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'main.js',
+            publicPath: ''
+      },
+      mode: 'development',
+      devServer: {
+        static: path.resolve(__dirname, './dist'),
+        compress: true,
+        port: 8080,
+
+        open: true
+      },
+      module: {
+        rules: [
+          {
+            test: /\.js$/,
+            use: 'babel-loader',
+            exclude: '/node_modules/'
+          }
+          ]
+      }
+   };
