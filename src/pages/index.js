8502c0e894d1071f378d8f18d292eb1bdc003ccb
Helga Ish <belfegor.nomnom@gmail.com>
2023-03-26 12:27:07 +0800

    fix: files paths changed

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,6 @@
-import './pages/index.css';
-import Card from './Card.js';
-import FormValidator from './FormValidator.js';
+import './index.css';
+import Card from '../components/Card.js';
+import FormValidator from '../components/FormValidator.js';
 import {
   initialCards,
   cardGallery,
@@ -11,11 +11,11 @@ import {
   editButton,
   addButton,
   allSelectors
-} from './constants.js';
-import Section from './Section.js';
-import PopupWithImage from './PopupWithImage.js';
-import PopupWithForm from './PopupWithForm.js';
-import UserInfo from './UserInfo.js';
+} from '../components/constants.js';
+import Section from '../components/Section.js';
+import PopupWithImage from '../components/PopupWithImage.js';
+import PopupWithForm from '../components/PopupWithForm.js';
+import UserInfo from '../components/UserInfo.js';
 
 // making card popup
 const cardPopupPreview = new PopupWithImage('#popup-card');
