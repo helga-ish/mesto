07bd0d9b296e83df66ca9b06ca07b9248c55409e
Helga Ish <belfegor.nomnom@gmail.com>
2023-03-26 12:15:51 +0800

    fix: handleEscClose rmv event listnr fixed

--- src/Popup.js
+++ src/Popup.js
@@ -1,18 +1,15 @@
 export default class Popup {
     constructor(popupSelector) {
         this._popupSelector = document.querySelector(popupSelector);
+        this._handleEscClose = this._handleEscClose.bind(this);
     }
     openPopup() {
         this._popupSelector.classList.add('popup_active');
-        document.addEventListener('keydown', (evt) => {
-            this._handleEscClose(evt);
-        });
+        document.addEventListener('keydown', this._handleEscClose);
     }
     closePopup() {
         this._popupSelector.classList.remove('popup_active');
-        document.removeEventListener('keydown', (evt) => {
-            this._handleEscClose(evt);
-        });
+        document.removeEventListener('keydown', this._handleEscClose);
     }
     _handleEscClose(evt) {
         if (evt.key === 'Escape') {
