e32710c89264679035705cc9420aed3c6dd5c52b
Helga Ish <belfegor.nomnom@gmail.com>
2023-04-02 18:52:03 +0800

    feat: delete card added

--- src/components/api.js
+++ src/components/api.js
@@ -74,4 +74,18 @@ export default class Api {
             return Promise.reject('ошибка');
         });
     }
+
+    deleteCard(cardId) {
+        return fetch(`${this._url}cards/${cardId}`, {
+            method: 'DELETE',
+            headers: this._headers,
+        }).then((res) => {
+            if (res.ok) {
+                return res.json();
+            }
+            return Promise.reject('ошибка');
+        });
+    }
+
+    
 }
