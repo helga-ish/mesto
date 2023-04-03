3ebe54166ff55c9c540b55e641903b7104a94861
Helga Ish <belfegor.nomnom@gmail.com>
2023-04-03 18:40:15 +0800

    feat: like quantity, count, isLiked added

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,12 +1,13 @@
 // create card
 export default class Card {
-    constructor({data, userId, handleCardClick, handleCardDeleteIcon}, templateSelector) {
+    constructor({data, userId, handleCardClick, handleCardDeleteIcon, handleLikeToggle}, templateSelector) {
         this._name = data.name;
         this._link = data.link;
         this._handleCardClick = handleCardClick;
         this._handleCardDeleteIcon = handleCardDeleteIcon;
+        this._handleLikeToggle = handleLikeToggle;
         this._templateSelector = templateSelector;
-        // this._cardId = data.id;
+        this._likes = data.likes;
         this._ownerId = data.owner._id;
         this._userId = userId;
     }
@@ -29,7 +30,9 @@ export default class Card {
       this._element.querySelector('.card__name').textContent = this._name;
 
       this._likeButton = this._element.querySelector('.card__like');
+
       this._removeButton = this._element.querySelector('.card__remove');
+      this._likeCount = this._element.querySelector('.card__like-count');
       this._setEventListeners();
       return this._element;
     }
@@ -39,6 +42,7 @@ export default class Card {
         this._handleCardClick(this._name, this._link);
       });
       this._likeButton.addEventListener('click', (event) => {
+        this._handleLikeToggle();
         this._handlePutLike(event);
       });
       if (this._ownerId !== this._userId) {
@@ -47,14 +51,25 @@ export default class Card {
       this._removeButton.addEventListener('click', () => {
         this._handleCardDeleteIcon();
       })
+      this.showLikesQuantity();
     }
 
     _handlePutLike(event) {
       event.target.classList.toggle("card__like_active");
     }
+    
 
     handleRemoveCard() {
       this._element.remove();
     }
 
+    showLikesQuantity() {
+      this._likeCount.textContent = this._likes.length;
+    }
+
+    isLiked() {
+      if(this._likeButton.classList.contains("card__like_active")) {
+        return true;
+      } else {return false};
+    }
 }
